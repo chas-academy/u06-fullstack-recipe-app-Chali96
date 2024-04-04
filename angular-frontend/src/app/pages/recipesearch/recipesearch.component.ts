1c5b0ef4bf5a67a137db6f2cb04f58ef32428a54
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-04 12:31:57 +0200

    code added to recipesearch.component.ts

--- angular-frontend/src/app/pages/recipesearch/recipesearch.component.ts
+++ angular-frontend/src/app/pages/recipesearch/recipesearch.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-recipesearch',
@@ -8,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './recipesearch.component.css'
 })
 export class RecipesearchComponent {
+  recipes?: any;
+
+  constructor(private recipeService: RecipeService) {}
+
+
+  searchRecipe() {
+    this.recipeService.getRecipes('chicken').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+        return {
+          self: item._links.self.href,
+          label: item.recipe.label,
+          image: item.recipe.image,
+          totalTime: item.recipe.totalTime,
+          ingredientLines: item.recipe.ingredientLines
+        }
+      });
+      
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
 
 }
