ba64fcaf50fabfe0b78d1f0995156b06f07d8e17
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-04 16:16:45 +0200

    buttons to different mealtypes added to recipe component

--- angular-frontend/src/app/pages/recipe/recipe.component.ts
+++ angular-frontend/src/app/pages/recipe/recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-recipe',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
+
 export class RecipeComponent {
+  recipes?: any;
+
+  constructor(private recipeService: RecipeService) {}
+
+
+  searchRecipe() {
+    this.recipeService.getRecipes('').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+        return {
+          self: item._links.self.href,
+          label: item.recipe.label,
+          image: item.recipe.image,
+          totalTime: item.recipe.totalTime,
+          ingredientLines: item.recipe.ingredientLines
+        }
+      });
+      
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
 
 }
