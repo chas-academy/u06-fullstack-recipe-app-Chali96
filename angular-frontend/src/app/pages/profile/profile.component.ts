a2a2ceb43cea0aa8d4c5cf92ac1b0cf3dc37fafe
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-04 12:15:37 +0200

    code added to profile.component.ts

--- angular-frontend/src/app/pages/profile/profile.component.ts
+++ angular-frontend/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+
+  user: User;
+
+  constructor(private auth: AuthService) {
+    this.user = {
+      id:-1,
+      name:"",
+      email:""
+    }
+  }
+
+  getUser(){
+    this.auth.getUser2().subscribe(res => {
+      console.log(res[0]);
+      this.user = res[0];
+    })
+  }
+
 
 }
