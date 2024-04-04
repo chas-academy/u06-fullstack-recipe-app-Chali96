bcc4c479c8f8b9bb881e114401a16a3e2b7d9aad
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-04 23:18:04 +0200

    added a contact

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RecipesearchComponent } from './pages/recipesearch/recipesearch.compone
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 export const routes: Routes = [
     {path: 'search', component: RecipesearchComponent},
     {path: 'recipe', component: RecipeComponent},
+    {path: 'contact', component: ContactComponent},
     {path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
 ];
