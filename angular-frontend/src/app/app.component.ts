7fc562b390adfaa8190dfadcc8255efad78510fa
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-04 13:28:02 +0200

    added code to app component.ts

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { LoginDetails } from './interfaces/login-details';
+import { User } from './interfaces/user';
+import { Observable } from 'rxjs';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-frontend';
+  title = 'authapp';
+
+  loginDetails: LoginDetails;
+
+  loggedIn$: Observable<boolean>;
+
+  constructor(private auth: AuthService){
+    this.loginDetails = {
+      email:"seb@seb.seb",
+      password:"sebsebseb"
+    }
+
+    this.loggedIn$ = this.auth.loggedIn$;
+  }
+
+  login(){
+    this.auth.loginUser(this.loginDetails);
+  }
+  logout(){
+    this.auth.logOut();
+  }
 }
