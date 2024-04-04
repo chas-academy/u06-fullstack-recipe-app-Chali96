eaee54371ff7a0037194ed659fa52bfeee7230af
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-04 12:00:56 +0200

    code added to auth.guard.ts

--- angular-frontend/src/app/guards/auth.guard.ts
+++ angular-frontend/src/app/guards/auth.guard.ts
@@ -1,5 +1,13 @@
 import { CanActivateFn } from '@angular/router';
+import { Inject, inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const auth: AuthService = inject(AuthService);
+
+  console.log(auth.getLoginStatus());
+
+
+  return auth.getLoginStatus();
 };
