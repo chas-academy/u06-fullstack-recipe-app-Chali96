724efe534bf4ec115da064fb11f24e8e7b5d4501
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-05 15:32:25 +0200

    added home to app.routes

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 export const routes: Routes = [
-    {path: '', component: RecipesearchComponent},
+    {path: '', component: HomeComponent},
     {path: 'recipe', component: RecipeComponent},
     {path: 'contact', component: ContactComponent},
+    {path: 'search', component: RecipesearchComponent},
     {path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    // {path: 'recipe:id', component: RecipeComponent}
 ];
