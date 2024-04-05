62fd3aad5b536ebe4cd08d427fd212b13ea0b6a7
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-05 15:45:31 +0200

    added login to router and linked in navbar

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path: 'recipe', component: RecipeComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'search', component: RecipesearchComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
     // {path: 'recipe:id', component: RecipeComponent}
 ];
