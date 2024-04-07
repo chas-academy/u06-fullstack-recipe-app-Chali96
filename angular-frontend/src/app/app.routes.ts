07e66052135df2b4d57e0a1597765e9e08303c68
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-07 12:33:30 +0200

    recipe/id added to app.routes

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -11,11 +11,11 @@ import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'recipe', component: RecipeComponent},
+    // {path: 'recipe', component: RecipeComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'search', component: RecipesearchComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
-    {path: 'recipe:id', component: RecipeComponent}
+    {path: 'recipe/:id', component: RecipeComponent}
 ];
