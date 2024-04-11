36fe88375b74ab7935ef757684f37389adf40740
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-11 14:45:07 +0200

    deleted contact component

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { RecipesearchComponent } from './pages/recipesearch/recipesearch.compone
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
-import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -12,7 +11,6 @@ import { RegisterComponent } from './pages/register/register.component';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     // {path: 'recipe', component: RecipeComponent},
-    {path: 'contact', component: ContactComponent},
     {path: 'search', component: RecipesearchComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
