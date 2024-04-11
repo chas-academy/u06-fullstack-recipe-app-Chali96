436fd4b0afff3ae13c4bde811cc70dbacd465586
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-11 11:46:47 +0200

    home page almost done

--- angular-frontend/src/app/pages/home/home.component.ts
+++ angular-frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,237 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../interfaces/recipe';
+import { RecipeService } from '../../services/recipe.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { RecipeidformatterPipe } from '../../pipes/recipeidformatter.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink, RecipeidformatterPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  recipes?: any;
+
+  constructor (private recipeService: RecipeService) {}
+  
+  searchRecipe() {
+    this.recipeService.getRecipes('chicken').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+  breakfastRecipe() {
+    this.recipeService.getRecipes('breakfast').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+
+  lunchRecipe() {
+    this.recipeService.getRecipes('lunch').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+  dinnerRecipe() {
+    this.recipeService.getRecipes('dinner').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+  dessertRecipe() {
+    this.recipeService.getRecipes('dessert').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+  snackRecipe() {
+    this.recipeService.getRecipes('snack').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+
+  vegetarianRecipe() {
+    this.recipeService.getRecipes('vegetarian').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+
+
+  veganRecipe() {
+    this.recipeService.getRecipes('vegan').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+  
+  dairyRecipe() {
+    this.recipeService.getRecipes('dairy').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+  
+  glutenRecipe() {
+    this.recipeService.getRecipes('gluten').subscribe((res) => {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+
+      let recipes = recipeArray.map(item => {
+          return {
+            self: item._links.self.href,
+            label: item.recipe.label,
+            image: item.recipe.image,
+            totalTime: item.recipe.totalTime,
+            ingredientLines: item.recipe.ingredientLines
+          }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+  
 
 }
+
+
+
--- angular-frontend/src/app/services/auth.service.ts
+++ angular-frontend/src/app/services/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Logindetails } from '../interfaces/logindetails';
-import { BehaviorSubject, Observable, catchError, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, throwError } from 'rxjs';
 import { User } from '../interfaces/user';
 import { LoggedInUser } from '../interfaces/loggedinuser';
 
+interface ResultData {
+  token: string,
+  user: User,
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private loggedIn = new BehaviorSubject<LoggedInUser>({
     user: undefined,
     loginState: false,
@@ -17,7 +25,7 @@ export class AuthService {
 
   loggedIn$ = this.loggedIn.asObservable();
 
-  private baseUrl = 'https://u06-recipe-app-chali96.onrender.com/api/';
+  private baseUrl = 'http://127.0.0.1:8000/api/';
 
   private httpOptions = {
     headers: new HttpHeaders({
@@ -27,49 +35,49 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-   updateLoginState(loginState: LoggedInUser) {
+  private updateLoginState(loginState: LoggedInUser) {
     this.loggedIn.next(loginState);
-   }
-    getLoginStatus() {
+  }
+  getLoginStatus() {
     return this.loggedIn.value.loginState;
-   }
+  }
 
-   loginUser(loginDetails: Logindetails) {
+  loginUser(loginDetails: Logindetails) {
     this.http
       .post<any>(this.baseUrl + 'login', loginDetails, this.httpOptions)
       .pipe(catchError(this.handleError))
-      .subscribe(result => {
+      .subscribe((result) => {
         console.log(result);
         this.updateLoginState({
           user: result.user,
           loginState: true,
         });
+
         this.httpOptions.headers = this.httpOptions.headers.set(
-          'Authorization', 
-          'Bearer'  + result.token
-          );
-      });
-
-   }
-
-   logoutUser() {
-    this.http.post<any>(this.baseUrl+'logout', {}, this.httpOptions).pipe(
-      catchError(this.handleError)).subscribe(result => {
-        console.log(result);
-        this.updateLoginState({
-          user: {
-            id: 0,
-            name: '',
-            email: '',
-            created_at: '',
-          },
-          loginState: false,
-        });
-        this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer");
+          'Authorization',
+          'Bearer' + result.token
+        );
       });
   }
 
-   getCurrentUser(){
+  logoutUser() {
+    this.http
+      .post<any>(this.baseUrl + 'logout', {}, this.httpOptions)
+      .pipe(catchError(this.handleError))
+      .subscribe((result) => {
+        console.log(result);
+        this.updateLoginState({
+          user: result.user,
+          loginState: false,
+        });
+        this.httpOptions.headers = this.httpOptions.headers.set(
+          'Authorization',
+          'Bearer'
+        );
+      });
+  }
+
+  getCurrentUser() {
     let user: User;
     user = {
       id: 0,
@@ -79,18 +87,14 @@ export class AuthService {
     };
     this.http
       .get<User[]>(
-        this.baseUrl + 'getUser/' + this.loggedIn.value.user?.id,
+        this.baseUrl + 'getUser' + this.loggedIn.value.user?.id,
         this.httpOptions
       )
       .subscribe((res) => (user = res[0]));
     return user;
   }
 
-  //  getUser2(): Observable<User[]> {
-  //   return this.http.get<User[]>(this.baseUrl+'getuser/2', this.httpOptions);
-  //  }
-
-   private handleError(error: HttpErrorResponse) {
+  private handleError(error: HttpErrorResponse) {
     if (error.status === 404) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
@@ -98,10 +102,12 @@ export class AuthService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
     }
     return throwError(
       () => new Error('Something bad happened; please try again later!')
-      );
-   }
+    );
+  }
 }
