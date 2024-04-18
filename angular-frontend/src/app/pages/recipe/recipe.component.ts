652d1024bf137d4add29de83e08382e2003d1d69
Chali Mohamedsani <chalisani@hotmail.com>
2024-04-18 21:42:58 +0200

    doneee with displaying recipe!!

--- angular-frontend/src/app/pages/recipe/recipe.component.ts
+++ angular-frontend/src/app/pages/recipe/recipe.component.ts
@@ -3,30 +3,41 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { RecipeService } from '../../services/recipe.service';
 import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
+import { Recipe } from '../../interfaces/recipe';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
 
 export class RecipeComponent implements OnInit {
-  id?: string;
-recipes: any;
+  recipeId: string | null | undefined;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService ) {}
+  allRecipes: any;
 
-  ngOnInit(): void {
-    this.route.paramMap.subscribe((params: ParamMap) => {
-      this.id = String(params.get('id'));
-      this.recipeService.getRecipe(this.id).subscribe(recipe => {
-        console.log(recipe)
-      });
-        
-    })
+  recipe: any;
 
+  constructor(private recipeService: RecipeService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.recipeId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.fetchRecipeData();
+  }
+
+  fetchRecipeData() {
+    if (!this.recipeId) {
+      return;
+    }
+
+    this.recipeService.getRecipe(this.recipeId).subscribe((result: any) => {
+      this.allRecipes = result;
+
+      let recipeDetails = Object.values(this.allRecipes).map((res: any) => res)
+      this.recipe = recipeDetails[0];
+    });
   }
 
 }
